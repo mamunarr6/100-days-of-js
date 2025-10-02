364ec0a23dea8ed9615bc5398acd0fc0b198116b
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-02 16:06:18 +0600

    js condition practiced if else and teranary operator

---
+++ day-2/condition.js
@@ -0,0 +1,15 @@
+/**
+ * Javascript Condition from ph books
+ */
+
+let partyMember = 120;
+let beingGift = true
+if(partyMember > 100 && beingGift === true){
+	console.log("Lets party all night.")
+}else{ console.log("I will party with myself")}
+
+// ternary operator
+
+let sunny = false;
+
+sunny === false? console.log("stay at home"): console.log("go outside")
