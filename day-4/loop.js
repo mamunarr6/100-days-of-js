a79c31f366d853d5ca2c2054d9b64fdf92f81d88
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-04 17:29:08 +0600

    while loop and for loop with sum and condition

--- day-4/loop.js
+++ day-4/loop.js
@@ -1,4 +1,31 @@
+let num = 0;
+let sum = 0;
+
+// for of
 const week = ["Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"];
 for(const day of week){
-    console.log(day)
+    // console.log(day)
 }
+
+// while loop
+while(num<=10){
+    // console.log(numbers)
+    sum = sum + num;
+    num++;
+}
+// console.log("The sum of first 10 numbers = ",sum)
+
+// for loop
+for(let i = 25;i<=75;i++){
+    // console.log(i)
+    sum += i;
+}
+// console.log("The sum of 25 to 75 is = ", sum);
+
+// print the numbers from 1 to 50 which are divided by 3 and 4 and the sum of those numbers;
+for(let i=1;i<51;i++){
+    if(i%3==0 && i%4==0){
+        sum += i;
+    }
+}
+console.log("the sum = ",sum)
