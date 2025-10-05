f88ad2cbb9778697a9a3e6385318fd188390e9c9
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-05 16:42:02 +0600

    more 3 problem solved

--- day-5/objects.js
+++ day-5/objects.js
@@ -25,3 +25,52 @@ for(const key of keys){
     const value =building[key]
     // console.log(key,value)
 }
+
+
+let students = [
+    { name: "Rahim", marks: 78 },
+    { name: "Karim", marks: 88 },
+    { name: "Nabila", marks: 95 },
+    { name: "Hasan", marks: 67 }
+];
+let topStudent = students[0];
+
+for(const student of students){
+    if(student.marks>topStudent.marks){
+        topStudent= student
+    }
+}
+
+// console.log("the higest mark is ",topStudent.marks)
+
+
+let studentss = [
+  { name: "Robin", class: "A" },
+  { name: "Aisha", class: "B" },
+  { name: "Rahim", class: "A" },
+  { name: "Karim", class: "C" },
+  { name: "Nabila", class: "B" }
+];
+
+let grouped = studentss.reduce((group,student)=>{
+    let cls = student.class;
+
+    if(!group[cls]){
+        group[cls]=[];
+        // console.log(group[cls])
+    }
+
+    group[cls].push(student);
+    return group;
+},{})
+// console.log(grouped)
+
+
+const numbers =[1,2,3,4,5,6,7,8,9];
+
+let addition = numbers.reduce((sum,num)=>{
+     sum = sum + num;
+    return sum;
+})
+
+// console.log(addition)
