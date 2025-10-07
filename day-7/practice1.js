dee5bfc1192c22d5bcd4e24ddc101df363ded96b
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-07 23:23:08 +0600

    add 1 more problem

--- day-7/practice1.js
+++ day-7/practice1.js
@@ -1,3 +1,6 @@
+/**
+**Practice: From ph book**
+ */
 
 function inchToFeet(inch){
     const feetFraction = inch/12;
@@ -7,9 +10,26 @@ function inchToFeet(inch){
     return feetNumber +" ft "+remainingFeet+" inch";
 }
 // console.log(inchToFeet(86));
+
+/** 1. Two yards make one fathom. So, write a function that will take yards as input and return how many fathoms that equals. For example, how many fathoms are in 13 miles? */
 function mileToGoz(mile){
     const goz= mile*1760;
 
     return goz + " goz";
 }
 // console.log(mileToGoz(13))
+
+/**2. Shurot is dieting. He’s thinking about calorie control. 1 kilowatt-hour of energy equals 860 kilocalories. So, write a function that can convert from kilowatts to kilocalories or vice versa. */
+function kilowattToKilocalori(kilowatt){
+    const kilocalories = kilowatt * 860;
+    return kilocalories;
+}
+// console.log(kilowattToKilocalori(8))
+
+/**3. You already know how to convert hours. But how many minutes or seconds are in one hour? I won’t tell you that — you have to think and then write a function that can convert between them. */
+function hourToMinutes(hour){
+    const minutes = hour * 60;
+
+    return minutes + " min";
+}
+// console.log(hourToMinutes(3.5));
