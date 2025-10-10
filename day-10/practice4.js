dbb2fde78362add7e9bef41c53531c352d377856
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-10 23:57:23 +0600

    getNamaSalay

--- day-10/practice4.js
+++ day-10/practice4.js
@@ -5,4 +5,14 @@ function getSquare(number){
         return 1;
     }
 }
-console.log(getSquare(5))
+// console.log(getSquare(5))
+function getNameSalary(name,salary){
+    if(!name){
+        return "Anonymus:" + salary +"BDT";
+    }else if(!salary){
+        return name + " " + 0 +"BDT";
+    }else{
+        return name + ": " + salary +"BDT";
+    }
+}
+// console.log(getNameSalary("Badal",))
