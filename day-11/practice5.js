dc2968299d4e821902c18ffe44db48889b7507c5
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-11 22:24:01 +0600

    pureSalary

--- day-11/practice5.js
+++ day-11/practice5.js
@@ -6,3 +6,12 @@ function favouriteBook(book=["Js Book"]){
     return "My favourite book = " + books;
 }
 // console.log(favouriteBook(["Math","English","Bangla"]));
+function pureSalary(salaryTax={salary:50000,tax:10}){
+    const {salary,tax} = salaryTax;
+    const taxAmount= (tax/100)*salary;
+    const getSalary = salary - taxAmount;
+    return getSalary;
+}
+
+const object = {salary:70000,tax:11};
+// console.log(pureSalary());
