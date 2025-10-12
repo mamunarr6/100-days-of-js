efe142fc562c12be40a1dd8eb0d763827b213dd8
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-12 13:11:41 +0600

    templateString

---
+++ day-12/templateString.js
@@ -0,0 +1,7 @@
+const animals = ["Cat","Dog","Elephant"];
+const result = `My favourite animals are ${animals.join()}`;
+// console.log(result);
+
+const person = {firstName:"Amit",lastName:"Kumar"};
+const personName = `Full Name : ${person.firstName} ${person.lastName}`;
+// console.log(personName)
