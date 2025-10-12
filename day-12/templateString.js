14b6fa7628b4c8bb387e42f21ab5b26156fa1117
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-12 13:24:33 +0600

    resultOfStudent

--- day-12/templateString.js
+++ day-12/templateString.js
@@ -5,3 +5,15 @@ const result = `My favourite animals are ${animals.join()}`;
 const person = {firstName:"Amit",lastName:"Kumar"};
 const personName = `Full Name : ${person.firstName} ${person.lastName}`;
 // console.log(personName)
+
+const student = {name:"Hridoy Hasan",age:20,results:[75,85,95]};
+function resultAverage(results){
+    let sum=0;
+    for(const mark of results){
+        sum = sum + mark;
+    }
+    return average= sum/results.length;
+}
+const averageMarks = resultAverage(student.results);
+const resultOfStudent = `${student.name} got average ${averageMarks} marks`;
+// console.log(resultOfStudent)
