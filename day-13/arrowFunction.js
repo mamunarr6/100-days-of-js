86246c42061e113bc82f83e487aab13421ab27ea
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-13 11:18:45 +0600

    arrowFunction added

---
+++ day-13/arrowFunction.js
@@ -0,0 +1,5 @@
+const divideMoney =(object)=>object.money/5;
+console.log(divideMoney({name:"Karim",money:200}));
+
+const firstLastSum = (array)=> array[0]+array[array.length-1];
+console.log(firstLastSum([5,4,8,6]))
