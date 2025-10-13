892a180a0d89aaeee6c6d826ce5d42786bed071a
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-13 11:22:11 +0600

    more arrow

--- day-13/arrowFunction.js
+++ day-13/arrowFunction.js
@@ -3,3 +3,6 @@ console.log(divideMoney({name:"Karim",money:200}));
 
 const firstLastSum = (array)=> array[0]+array[array.length-1];
 console.log(firstLastSum([5,4,8,6]))
+
+const sumOfTwo = (a=10,b=5)=>a+b;
+console.log(sumOfTwo(25,52))
