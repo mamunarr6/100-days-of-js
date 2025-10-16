926796c070cfb2476deb09ab83012d24749025d5
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-16 23:49:48 +0600

    getAverage by rest operator

--- day-14/rest.js
+++ day-14/rest.js
@@ -7,3 +7,12 @@ const getSum = ([first,second, ...rest])=>{
     return sum;
 }
 // console.log(getSum(numbers));
+
+const getAverage = (...number)=>{
+    let sum = 0;
+    number.forEach(num=>{
+        sum = sum + num;
+    })
+    return sum/number.length;
+}
+// console.log(getAverage(10,20,15,25,30));
