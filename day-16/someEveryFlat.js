89abbaa33c8058b2605598d2eab4aee928f644d7
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-19 23:04:18 +0600

    some method used

--- day-16/someEveryFlat.js
+++ day-16/someEveryFlat.js
@@ -4,4 +4,7 @@ const numbers = [39,59,32,94,99,29,103,106];
 
 const number = [40,60,30,90,100,20,120,110];
 // every method
-console.log(number.every(num=>num%5===0));
+// console.log(number.every(num=>num%5===0));
+
+const words = ["Mamun","Rahad","Karimullah","Jasim Mia", "Shakib"];
+console.log(words.some(word=>word.length==="hello".length))
