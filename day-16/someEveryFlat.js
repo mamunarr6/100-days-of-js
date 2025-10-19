0c1ed4fd4fd5993f7688f5d9f8c9ec0554aa2eaa
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-19 20:55:12 +0600

    every method used

--- day-16/someEveryFlat.js
+++ day-16/someEveryFlat.js
@@ -1,2 +1,7 @@
 const numbers = [39,59,32,94,99,29,103,106];
-console.log(numbers.some(number=>number>=100))
+// some method
+// console.log(numbers.some(number=>number>=100))
+
+const number = [40,60,30,90,100,20,120,110];
+// every method
+console.log(number.every(num=>num%5===0));
