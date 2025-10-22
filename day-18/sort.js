a5d8b08cb2db2476d639c8ee4d09281e226079c0
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-22 22:50:32 +0600

    sortedAge

--- day-18/sort.js
+++ day-18/sort.js
@@ -1,2 +1,5 @@
 const friends = ["Munna","Rakib","Abdul","Babul","Chitra"];
 // console.log(friends.sort());
+const ages = [50,12,25,8,15];
+const sortedAge = ages.sort((a,b)=>a-b);
+console.log(sortedAge);
