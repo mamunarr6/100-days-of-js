603a0755ca92fbb4a09fef4796c0eb8b06830eb1
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-22 22:57:11 +0600

    sorted friends by age of object in array

--- day-18/sort.js
+++ day-18/sort.js
@@ -1,5 +1,14 @@
-const friends = ["Munna","Rakib","Abdul","Babul","Chitra"];
-// console.log(friends.sort());
+const friend = ["Munna","Rakib","Abdul","Babul","Chitra"];
+// console.log(friend.sort());
 const ages = [50,12,25,8,15];
 const sortedAge = ages.sort((a,b)=>a-b);
-console.log(sortedAge);
+// console.log(sortedAge);
+
+const friends = [
+    {name:"Babul",age:22},
+    {name:"Chitra",age:20},
+    {name:"Abdul",age:21},
+    {name:"Diba",age:21}
+]
+const sortedFriendsByAge=friends.sort((a,b)=>a.age-b.age);
+console.log(sortedFriendsByAge)
