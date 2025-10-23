04499888f9c9466a88ed77b413866c6599dc234e
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-23 23:42:58 +0600

    reverse used

---
+++ day-19/reverseSliceSplice.js
@@ -0,0 +1,15 @@
+const names = ["Ali","Badol","Chitto","Ditto","Efat"];
+names.reverse();
+// console.log(names);
+const numbers = [10,20,30,40,50];
+numbers.reverse();
+// console.log(numbers)
+const users =[
+    {name:"Ali",age:21},
+    {name:"Badol",age:22},
+    {name:"Chitto",age:20},
+    {name:"Efat",age:22},
+    {name:"Ditto",age:23}
+]
+users.reverse();
+console.log((users))
