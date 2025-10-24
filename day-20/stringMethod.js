7b32bfb8e98f6d23ef620d3da19df970ed7e1f58
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-24 23:50:05 +0600

    Immutable practised

---
+++ day-20/stringMethod.js
@@ -0,0 +1,12 @@
+const city = "Chattogram";
+// console.log(city.indexOf("g"));
+const division = "Sylhet";
+// console.log(division.includes("y"));
+const called = "Rifat";
+// console.log(called[called.length-1]);
+const language = "Javascript";
+// console.log(language.indexOf("rip"));
+const text = "Immutable";
+const length = text.length;
+text[0]="E"
+// console.log(text[0] === "E"? "Mutable":"Immutable");
