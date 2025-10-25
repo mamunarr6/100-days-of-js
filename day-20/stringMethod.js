3881132d53f597ac2a66e7d56fc4a4561f9f0338
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-25 23:57:59 +0600

    lowercase uppercase

--- day-20/stringMethod.js
+++ day-20/stringMethod.js
@@ -10,3 +10,14 @@ const text = "Immutable";
 const length = text.length;
 text[0]="E"
 // console.log(text[0] === "E"? "Mutable":"Immutable");
+
+const subject = 'Chemistry';
+const book = 'chemistry';
+
+if (subject === book) {
+console.log('i am reading book');
+} else {
+console.log('hudai prista ultai');
+}
+
+
