bd0e63efe264e6ac3d24ef75bb6bb5b5bbd2b6a7
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-28 00:09:58 +0600

    toString method and reverse practised

--- day-21/stringMethod2.js
+++ day-21/stringMethod2.js
@@ -4,3 +4,13 @@ const string = "Hi!";
 const string2 = "I am learning JavaScript. JavaScript is amazing!";
 // console.log(string2.replace("JavaScript","JS"));
 // console.log(string2.replaceAll("JavaScript","JS"));
+
+const num = 123;
+// console.log(num.toString());
+
+const sentence = "I am learning Web Dev";
+let reverse = " ";
+for(const letter of sentence){
+    reverse = letter + reverse;
+}
+// console.log(reverse)
