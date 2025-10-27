47aaa9cc08e88fe5a6a03a9a4e57f51105592705
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-27 23:58:19 +0600

    replace and replaceAll method used

--- day-21/stringMethod2.js
+++ day-21/stringMethod2.js
@@ -1,2 +1,6 @@
 const string = "Hi!";
 // console.log(string.repeat(10));
+
+const string2 = "I am learning JavaScript. JavaScript is amazing!";
+// console.log(string2.replace("JavaScript","JS"));
+// console.log(string2.replaceAll("JavaScript","JS"));
