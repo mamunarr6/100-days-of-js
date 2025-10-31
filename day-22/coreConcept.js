1ce74c10cc41963b0ecb7da3ea86ab0de7eff929
Mamun Ar Roshid <mamunarr6@gmail.com>
2025-10-31 23:59:22 +0600

    primitive and non-primitive practiced

--- day-22/coreConcept.js
+++ day-22/coreConcept.js
@@ -1,2 +1,13 @@
 const excuse = "Today 30th October was my Exam . So I was in pressure that's why I can't practice yesterday and today."
 // console.log(excuse);
+
+let num = 15;
+let copy = num;
+copy = 25;
+console.log("num: ",num, " copy: ",copy);
+
+let language = { name: "js",age: 30};
+let updateLanguage = language;
+updateLanguage.key = "location";
+updateLanguage.value = "Browser";
+console.log("language: ",language, " updateLanguage: ",updateLanguage);
